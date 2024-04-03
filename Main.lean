a83a88abadc759d8c40f238c447a8354c7796acd
Lean 4 VS Code Extension <>
2024-04-03 09:21:17 +0200

    Initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import «Hashmap»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package «hashmap» where
+  -- add package configuration options here
+
+lean_lib «Hashmap» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «hashmap» where
+  root := `Main
