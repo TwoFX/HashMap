6da0f1873c14539b24c2a55bd9d190b0dd344514
Markus Himmel <markus@lean-fro.org>
2024-06-11 13:07:37 +0200

    Reorganize more

---
+++ Hashmap/DHashMap/Raw.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2018 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura, Mario Carneiro, Markus Himmel
+-/
+import Hashmap.AssocList.Basic
+
+set_option autoImplicit false
+
+universe u v
+
+namespace MyLean.DHashMap
+
+structure Raw (α : Type u) (β : α → Type v) where
+  size : Nat
+  buckets : Array (AssocList α β)
+
+end MyLean.DHashMap
